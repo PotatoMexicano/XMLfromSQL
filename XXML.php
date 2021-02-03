8b9ff98e74672305be7a61aa84629e6a19199051
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2021-02-03 15:50:45 -0300

    Update XXML.php

--- XXML.php
+++ XXML.php
@@ -1,42 +1,50 @@
 <?php
 
-require_once("../Controllers/Conexao.php"); // EDIT THIS
-$table1 = "registros"; //CHANGE THIS BY TABLE NAME
-header('Content-Type: text/xml');
+class XMLfromSQL
+{
+public static function generate($table,$connection){
+    
+    //How to use: 
+    // another .php call for: $xml = XMLfromSQL::generate("table_name",$connection); 
+    //Use to show result: echo $xml->asXML();
+    
+    header('Content-Type: text/xml');
+    //header('Content-Type: text/html; charset=utf-8');
 
-$connection = Conexao::getConnection(); //ADAPT THIS
+    $stmt = $connection->query("SELECT * FROM " . $table);
+    $keys = $stmt->fetch(PDO::FETCH_ASSOC);
+    // get all columns from table
+    $size = count(array_keys($keys)) - 1;
+    $arr = array_keys($keys);
 
-$stmt = $connection->query("SELECT * FROM ".$table1);
-$keys = $stmt->fetch(PDO::FETCH_ASSOC);
-// get all clumns from table
-$size = count(array_keys($keys))-1;
-$arr = array_keys($keys);
 
-$str = '';
-$first_column = '';
+    $str = '';
+    $first_column = '';
 
-for ($i = 0; $i <=$size; $i++) {
-    if($i == $size){
-        $str .=  $arr[$i];
-    }else{
-        if($i == 0){
-            $first_column = $arr[$i];
+    for ($i = 0; $i <= $size; $i++) {
+        if ($i == $size) {
+            $str .= $arr[$i];
+        } else {
+            if ($i == 0) {
+                $first_column = $arr[$i];
+            }
+            $str .= $arr[$i] . ",";
         }
-        $str .= $arr[$i].",";
     }
-}
-//for make string for next query
-    $stmt = $connection->query("SELECT ".$str." FROM " . $table1);
-//execute select with columns from frist select
-    $xml = new SimpleXMLElement('<' . $table1 . '/>');
+    //for make string for next query
+    $stmt = $connection->query("SELECT " . $str . " FROM " . $table);
+    //execute select with columns from first select
+    $xml = new SimpleXMLElement('<' . $table . '/>');
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $add = $xml->addChild('item'.$row[$first_column]);
-        //mount item with 'item' + id from frist column
+        $add = $xml->addChild('item' . $row[$first_column]);
+        //mount item with 'item' + id from first column
 
         foreach ($row as $key => $value) {
             $add->addChild($key, htmlspecialchars($value));
             //add values in $add
         }
     }
-echo $xml->asXML();
+    return $xml;
     //mount xml on screen
+    }
+}
